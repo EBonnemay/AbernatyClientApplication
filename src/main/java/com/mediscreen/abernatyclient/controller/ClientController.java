de8e847ba7f4b5e059145d93e202a21f88af409c
Emmanuelle Bonnemay <e.bonnemay@live.fr>
2023-06-15 00:37:33 +0200

    update ok

--- src/main/java/com/mediscreen/abernatyclient/controller/ClientController.java
+++ src/main/java/com/mediscreen/abernatyclient/controller/ClientController.java
@@ -4,8 +4,10 @@ import com.mediscreen.abernatyclient.beans.PatientBean;
 import com.mediscreen.abernatyclient.proxies.MicroservicePatientsProxy;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+
+import java.sql.Date;
 import java.util.List;
 
 @Controller
@@ -15,11 +17,41 @@ public class ClientController {
     this.patientsProxy = patientsProxy;
     }
     @RequestMapping("/")
-    public String returnHomePage(Model model){
-        String test = "hello world";
+    public String displayHomePage(Model model){
+
         List<PatientBean> patients = patientsProxy.listOfPatients();
 
         model.addAttribute("patients", patients);
         return "HomePage";
     }
+    @GetMapping("/patient/update/{id}")
+    public String displayUpdateForm(@PathVariable("id")String id, Model model){
+        System.out.println("id is" + id);
+        PatientBean patient = patientsProxy.retrievePatient(Integer.parseInt(id));
+        //model.addAttribute("family", patient.getFamily());
+        //model.addAttribute("given", patient.getGiven());
+        System.out.println(patient.getDate_of_birth());
+        System.out.println(patient.getDate_of_birth() instanceof Date);
+        model.addAttribute("patient", patient);
+        return "UpdatePatient";
+    }
+
+    @PostMapping("/patient/update/{id}")
+    public String updatePatient(@PathVariable("id") String id, @RequestParam(name = "family", required = true) String family, @RequestParam(name = "given", required = true) String given, @RequestParam(name = "date_of_birth", required = false) Date dateOfBirth, @RequestParam(name = "sex", required = false) String sex, @RequestParam(name = "address", required = false) String address, @RequestParam(name = "phone", required = false) String phone, Model model){
+        System.out.println("this is family "+ family);
+        System.out.println("this is date :"+ dateOfBirth);
+        //j'ai une family et une date  format 1999-12-19
+
+       //Date dateOfBirth = Date.valueOf(stringDateOfBirth);
+        PatientBean patient = patientsProxy.updatePatient(id, family, given, dateOfBirth, sex, address, phone);
+        //ma date est nulle donc pas instance de Date
+        System.out.println("in client controller post : date of birth = "+ patient.getDate_of_birth());
+        System.out.println("in client controller post : date instance of Date?" + (patient.getDate_of_birth() instanceof Date));
+
+        //model.addAttribute("patient", patient);
+        //model.addAttribute("given", patient.getGiven());
+
+    return "redirect:/" ;
+    }
+
 }
--- src/main/java/com/mediscreen/abernatyclient/proxies/MicroservicePatientsProxy.java
+++ src/main/java/com/mediscreen/abernatyclient/proxies/MicroservicePatientsProxy.java
@@ -2,18 +2,26 @@ package com.mediscreen.abernatyclient.proxies;
 
 import com.mediscreen.abernatyclient.beans.PatientBean;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
+import java.sql.Date;
 import java.util.List;
 
 @FeignClient(name = "microservice-patients", url = "localhost:8080")
 public interface MicroservicePatientsProxy {
+    //@Operation(value="récupère la liste des patients")
     @GetMapping(value = "/patients")
     List<PatientBean> listOfPatients();
 
 
-    @GetMapping(value = "/Produits/{id}")
+    @GetMapping(value = "/patient/{id}")
     PatientBean retrievePatient(@PathVariable("id")int id);
+
+    @GetMapping(value ="/patient/{family}/{given}")
+    PatientBean retrievePatient(@PathVariable("family")String family, @PathVariable("given")String given);
+
+    @PostMapping(value = "/patient/update/{id}")
+    PatientBean updatePatient(@PathVariable("id") String id, @RequestParam(name = "family", required = true) String family, @RequestParam(name = "given", required = true) String given, @RequestParam(name = "date_of_birth", required = false) Date dateOfBirth, @RequestParam(name = "sex", required = false) String sex, @RequestParam(name = "address", required = false) String address, @RequestParam(name = "phone", required = false) String phone);
 
 }
